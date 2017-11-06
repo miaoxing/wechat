29ffaf58790ca2c29d5d81758ca8571a943e52a7
twinh <twinhuang@qq.com>
2017-11-06 14:46:11 +0800

    增加朋友券兑换库存的操作

--- src/Service/WechatApi.php
+++ src/Service/WechatApi.php
@@ -1973,6 +1973,40 @@ class WechatApi extends \miaoxing\plugin\BaseService
         });
     }
 
+    public function getCardPayCoinsInfo()
+    {
+        return $this->authRet(function () {
+           return $this->http([
+               'url' => $this->baseUrl . 'card/pay/getcoinsinfo?access_token=' . $this->accessToken,
+               'dataType' => 'json'
+           ]);
+        });
+    }
+
+    public function getCardPayPayPrice(array $data)
+    {
+        return $this->authRet(function () use ($data) {
+            return $this->http([
+                'url' => $this->baseUrl . 'card/pay/getpayprice?access_token=' . $this->accessToken,
+                'method' => 'post',
+                'dataType' => 'json',
+                'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
+            ]);
+        });
+    }
+
+    public function confirmCardPay(array $data)
+    {
+        return $this->authRet(function () use ($data) {
+            return $this->http([
+                'url' => $this->baseUrl . 'card/pay/confirm?access_token=' . $this->accessToken,
+                'method' => 'post',
+                'dataType' => 'json',
+                'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
+            ]);
+        });
+    }
+
     /**
      * 生成指定长度的随机字符串
      *
