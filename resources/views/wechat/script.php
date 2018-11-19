39f90f96056ca8f50c0771fee65a763f30cc0833
twinh <twinhuang@qq.com>
2018-11-19 18:07:13 +0800

    Merge branch 'master' into feature/jth\n\n# Conflicts:\n#\t.babelrc\n#\tpackage.json\n#\tvendor/miaoxing/app/modules/components/AreaCascading.js\n#\tvendor/miaoxing/app/modules/components/FormItemDateTimeRange.js\n#\tvendor/miaoxing/app/modules/components/SearchDateRangePicker.js\n#\tyarn.lock

--- resources/views/wechat/script.php
+++ resources/views/wechat/script.php
@@ -1,6 +1,6 @@
 <script>
   var wxInitUrl = <?= json_encode(wei()->request->getUrl()) ?>;
-  <?php if (!wei()->app->isAdmin() && !wei()->plugin->isInstalled('wxa')) { ?>
+  <?php if (!wei()->plugin->isInstalled('wxa')) { ?>
   var wxShare = <?= wei()->share->toWechatJson() ?>;
   require(['plugins/wechat/js/wx'], function (wx) {
     wx.load(function () {
