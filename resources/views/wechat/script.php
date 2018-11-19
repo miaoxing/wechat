17fb669a11cd0c663bfaea51cfb994fe03580aef
twinh <twinhuang@qq.com>
2018-11-19 21:45:56 +0800

    feat: 初步实现前台使用bs4

--- resources/views/wechat/script.php
+++ resources/views/wechat/script.php
@@ -1,8 +1,8 @@
 <script>
   var wxInitUrl = <?= json_encode(wei()->request->getUrl()) ?>;
-  <?php if (!wei()->plugin->isInstalled('wxa')) { ?>
+  <?php if (!wei()->app->isAdmin() && !wei()->plugin->isInstalled('wxa')) { ?>
   var wxShare = <?= wei()->share->toWechatJson() ?>;
-  require(['plugins/wechat/js/wx'], function (wx) {
+  window.require && require(['plugins/wechat/js/wx'], function (wx) {
     wx.load(function () {
       wx.onMenuShareTimeline(wxShare);
       wx.onMenuShareAppMessage(wxShare);
