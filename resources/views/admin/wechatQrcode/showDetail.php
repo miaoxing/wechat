7d7e3d1733050f291fe57702d198caf2aa634998
twinh <twinhuang@qq.com>
2019-02-21 14:43:23 +0800

    refactoring: 默认不加载jQuery UI

--- resources/views/admin/wechatQrcode/showDetail.php
+++ resources/views/admin/wechatQrcode/showDetail.php
@@ -119,7 +119,8 @@
 <?= $block->js() ?>
 <script>
   require(['plugins/stat/js/highcharts', 'jquery-unparam','form', 'plugins/admin/js/data-table',
-    'https://cdn.jsdelivr.net/npm/jqueryui@1.11.1/jquery-ui.min.js'
+    'https://cdn.jsdelivr.net/npm/jqueryui@1.11.1/jquery-ui.min.js',
+    'css!comps/jquery-ui-custom/jquery-ui-1.10.3.full.min.css',
   ], function () {
     $('#search-form').loadParams();
     // 渲染趋势图
